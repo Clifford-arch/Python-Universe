d68d6d687a8aac5c408e235c5d41618b140d1c72
Clifford <cliffordchristophermarx@gmail.com>
2026-01-04 18:50:09 +0530

    Arrays: Better method using two pointer for move_zeroes

--- arrays/move-zeroes.py
+++ arrays/move-zeroes.py
@@ -1,10 +1,19 @@
 """Move zeroes to the end of the array"""
 def move_zeroes(array):
-    n=len(array)-1
-    for i in range(n):
-        if array[i]==0:
-            array[i],array[i+1]=array[i+1],array[i]
+    # n= len(array)-1
+    # for i in range(n):
+    #     if array[i]==0:
+    #         array[i],array[i+1]=array[i+1],array[i]
     
+    # return array
+
+    """Two pointer solution"""
+
+    last_non_zero=0
+    for i in range(len(array)):
+        if array[i]!=0:
+            array[last_non_zero],array[i]=array[i],array[last_non_zero]
+            last_non_zero+=1
     return array
 
 print(move_zeroes([0,1,2,6,0]))
